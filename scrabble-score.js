c911e4908c226ce8bd0dff1d36cc39c96acabb0d
dom-m17 <dominicmaynard00@gmail.com>
2024-08-06 12:31:57 +0100

    initial test

--- scrabble-score.js
+++ scrabble-score.js
@@ -1,1 +1,5 @@
-export function calculateScrabbleScore(word) {}
+export function calculateScrabbleScore(word) {
+    if (word == 'a') {
+        return 1
+    }
+}
--- scrabble-score.test.js
+++ scrabble-score.test.js
@@ -1,1 +1,15 @@
 import { calculateScrabbleScore } from "./scrabble-score";
+import { test, expect } from "vitest"
+
+const onePoint = ['a', 'e', 'i', 'l', 'n', 'o', 'r', 's', 't', 'u']
+const twoPoint = ['d', 'g']
+const threePoint = ['b', 'c', 'm', 'p']
+const fourPoint = ['f', 'h', 'v', 'w', 'y']
+const fivePoint = ['k']
+const eightPoint = ['j', 'x']
+const tenPoints = ['q', 'z']
+
+// test to make sure 'a' returns a score of 1
+test('the word "a" returns a score of 1', () => {
+    expect(calculateScrabbleScore('a')).toBe(1)
+})

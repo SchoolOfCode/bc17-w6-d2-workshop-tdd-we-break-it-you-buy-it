77bb9b6fecf0f04b05b14817d82e5a60d5ac8ed4
Ryan <ryanshaunsmith@gmail.com>
2024-08-06 14:02:45 +0100

    updated test for onePoint and added some comments

--- scrabble-score.js
+++ scrabble-score.js
@@ -1,30 +1,28 @@
-export const onePoint = ['a', 'e', 'i', 'l', 'n', 'o', 'r', 's', 't', 'u']
-export const twoPoint = ['d', 'g']
-export const threePoint = ['b', 'c', 'm', 'p']
-export const fourPoint = ['f', 'h', 'v', 'w', 'y']
-export const fivePoint = ['k']
-export const eightPoint = ['j', 'x']
-export const tenPoint = ['q', 'z']
-
-
-
+export const onePoint = ["a", "e", "i", "l", "n", "o", "r", "s", "t", "u"];
+export const twoPoint = ["d", "g"];
+export const threePoint = ["b", "c", "m", "p"];
+export const fourPoint = ["f", "h", "v", "w", "y"];
+export const fivePoint = ["k"];
+export const eightPoint = ["j", "x"];
+export const tenPoint = ["q", "z"];
 
 export function calculateScrabbleScore(word) {
-    switch (word) {
-        case onePoint:
-            return 1
-        case twoPoint:
-            return 2
-        case threePoint:
-            return 3
-        case fourPoint:
-            return 4
-        case fivePoint:
-            return 5
-        case eightPoint:
-            return 8
-        case tenPoint:
-            return 10
-    }
-    
+  switch (word) {
+    // if letter is included in the onePoint array, return a value of 1
+    case onePoint:
+      return 1;
+    // if letter is included in the twoPoint array, return a value of 2
+    case twoPoint:
+      return 2;
+    case threePoint:
+      return 3;
+    case fourPoint:
+      return 4;
+    case fivePoint:
+      return 5;
+    case eightPoint:
+      return 8;
+    case tenPoint:
+      return 10;
+  }
 }
--- scrabble-score.test.js
+++ scrabble-score.test.js
@@ -1,31 +1,44 @@
-import { calculateScrabbleScore, onePoint, twoPoint, threePoint, fourPoint, fivePoint, eightPoint, tenPoint } from "./scrabble-score";
-import { test, expect } from "vitest"
+import {
+  calculateScrabbleScore,
+  onePoint,
+  twoPoint,
+  threePoint,
+  fourPoint,
+  fivePoint,
+  eightPoint,
+  tenPoint,
+} from "./scrabble-score";
+import { test, expect } from "vitest";
 
 // test to make sure 'a' returns a score of 1
-test(onePoint, 'the one point letters returns a score of 1', (r) => {
-    expect(calculateScrabbleScore(r)).toBe(1)
+test("the one point letters returns a score of 1", () => {
+  expect(
+    onePoint.forEach((e) => {
+      calculateScrabbleScore(e);
+    })
+  ).toBe(1);
 });
 
-test('the two point letters returns a score of 2', () => {
-    expect(calculateScrabbleScore(twoPoint)).toBe(2)
-})
+test("the two point letters returns a score of 2", () => {
+  expect(calculateScrabbleScore(twoPoint)).toBe(2);
+});
 
-test('the three point letters returns a score of 3', () => {
-    expect(calculateScrabbleScore(threePoint)).toBe(3)
-})
+test("the three point letters returns a score of 3", () => {
+  expect(calculateScrabbleScore(threePoint)).toBe(3);
+});
 
-test('the four point letters returns a score of 4', () => {
-    expect(calculateScrabbleScore(fourPoint)).toBe(4)
-})
+test("the four point letters returns a score of 4", () => {
+  expect(calculateScrabbleScore(fourPoint)).toBe(4);
+});
 
-test('the five point letters returns a score of 5', () => {
-    expect(calculateScrabbleScore(fivePoint)).toBe(5)
-})
+test("the five point letters returns a score of 5", () => {
+  expect(calculateScrabbleScore(fivePoint)).toBe(5);
+});
 
-test('the eight point letters returns a score of 8', () => {
-    expect(calculateScrabbleScore(eightPoint)).toBe(8)
-})
+test("the eight point letters returns a score of 8", () => {
+  expect(calculateScrabbleScore(eightPoint)).toBe(8);
+});
 
-test('the ten point letters returns a score of 10', () => {
-    expect(calculateScrabbleScore(tenPoint)).toBe(10)
-})
+test("the ten point letters returns a score of 10", () => {
+  expect(calculateScrabbleScore(tenPoint)).toBe(10);
+});

4a9b19bf76b0e41212f0255d2f746bedc518a58c
dom-m17 <dominicmaynard00@gmail.com>
2024-08-06 14:19:24 +0100

    test for one point array

--- scrabble-score.js
+++ scrabble-score.js
@@ -6,23 +6,8 @@ export const fivePoint = ["k"];
 export const eightPoint = ["j", "x"];
 export const tenPoint = ["q", "z"];
 
-export function calculateScrabbleScore(word) {
-  switch (word) {
-    // if letter is included in the onePoint array, return a value of 1
-    case onePoint:
-      return 1;
-    // if letter is included in the twoPoint array, return a value of 2
-    case twoPoint:
-      return 2;
-    case threePoint:
-      return 3;
-    case fourPoint:
-      return 4;
-    case fivePoint:
-      return 5;
-    case eightPoint:
-      return 8;
-    case tenPoint:
-      return 10;
+export function calculateScrabbleScore(letter) {
+  if (onePoint.includes(letter)) {
+    return 1
   }
 }
--- scrabble-score.test.js
+++ scrabble-score.test.js
@@ -11,34 +11,62 @@ import {
 import { test, expect } from "vitest";
 
 // test to make sure 'a' returns a score of 1
-test("the one point letters returns a score of 1", () => {
-  expect(
-    onePoint.forEach((e) => {
-      calculateScrabbleScore(e);
-    })
-  ).toBe(1);
-});
+// test("the one point letters returns a score of 1", () => {
+//   expect(
+//     onePoint.forEach((e) => {
+//       calculateScrabbleScore(e);
+//     })
+//   ).toBe(1);
+// });
 
-test("the two point letters returns a score of 2", () => {
-  expect(calculateScrabbleScore(twoPoint)).toBe(2);
-});
+test.each(onePoint)("the letter %s returns a score of 1", (letter) => {
+  expect(calculateScrabbleScore(letter)).toBe(1)
+})
 
-test("the three point letters returns a score of 3", () => {
-  expect(calculateScrabbleScore(threePoint)).toBe(3);
-});
+test.each(twoPoint)("the letter %s returns a score of 2", (letter) => {
+  expect(calculateScrabbleScore(letter)).toBe(2)
+})
 
-test("the four point letters returns a score of 4", () => {
-  expect(calculateScrabbleScore(fourPoint)).toBe(4);
-});
+test.each(threePoint)("the letter %s returns a score of 3", (letter) => {
+  expect(calculateScrabbleScore(letter)).toBe(3)
+})
 
-test("the five point letters returns a score of 5", () => {
-  expect(calculateScrabbleScore(fivePoint)).toBe(5);
-});
+test.each(fourPoint)("the letter %s returns a score of 4", (letter) => {
+  expect(calculateScrabbleScore(letter)).toBe(4)
+})
 
-test("the eight point letters returns a score of 8", () => {
-  expect(calculateScrabbleScore(eightPoint)).toBe(8);
-});
+test.each(fivePoint)("the letter %s returns a score of 5", (letter) => {
+  expect(calculateScrabbleScore(letter)).toBe(5)
+})
 
-test("the ten point letters returns a score of 10", () => {
-  expect(calculateScrabbleScore(tenPoint)).toBe(10);
-});
+test.each(eightPoint)("the letter %s returns a score of 8", (letter) => {
+  expect(calculateScrabbleScore(letter)).toBe(8)
+})
+
+test.each(tenPoint)("the letter %s returns a score of 10", (letter) => {
+  expect(calculateScrabbleScore(letter)).toBe(10)
+})
+
+// test("the two point letters returns a score of 2", () => {
+//   expect(calculateScrabbleScore(twoPoint)).toBe(2);
+// });
+
+// test("the three point letters returns a score of 3", () => {
+//   expect(calculateScrabbleScore(threePoint)).toBe(3);
+// });
+
+// test("the four point letters returns a score of 4", () => {
+//   expect(calculateScrabbleScore(fourPoint)).toBe(4);
+// });
+
+// test("the five point letters returns a score of 5", () => {
+//   expect(calculateScrabbleScore(fivePoint)).toBe(5);
+// });
+
+// test("the eight point letters returns a score of 8", () => {
+//   expect(calculateScrabbleScore(eightPoint)).toBe(8);
+// });
+
+// test("the ten point letters returns a score of 10", () => {
+//   expect(calculateScrabbleScore(tenPoint)).toBe(10);
+// });

130167227c0274080a990ca684099054417286fe
Jordan <jordanfhoughton@gmail.com>
2024-08-06 14:23:21 +0100

    ticket two done

--- scrabble-score.js
+++ scrabble-score.js
@@ -9,5 +9,23 @@ export const tenPoint = ["q", "z"];
 export function calculateScrabbleScore(letter) {
   if (onePoint.includes(letter)) {
     return 1
+  } 
+  else if (twoPoint.includes(letter)){
+    return 2
+  } 
+  else if (threePoint.includes(letter)){
+    return 3
   }
+    else if (fourPoint.includes(letter)){
+        return 4
+    }
+    else if (fivePoint.includes(letter)){
+        return 5
+    }
+    else if (eightPoint.includes(letter)){
+        return 8
+    }
+    else if (tenPoint.includes(letter)){
+        return 10
+    }
 }

d03b7e89a40de6f0bcc1087c51efe1165336f1d0
Jordan <jordanfhoughton@gmail.com>
2024-08-06 13:45:43 +0100

    added tests two point to ten point

--- scrabble-score.js
+++ scrabble-score.js
@@ -1,5 +1,30 @@
+export const onePoint = ['a', 'e', 'i', 'l', 'n', 'o', 'r', 's', 't', 'u']
+export const twoPoint = ['d', 'g']
+export const threePoint = ['b', 'c', 'm', 'p']
+export const fourPoint = ['f', 'h', 'v', 'w', 'y']
+export const fivePoint = ['k']
+export const eightPoint = ['j', 'x']
+export const tenPoint = ['q', 'z']
+
+
+
+
 export function calculateScrabbleScore(word) {
-    if (word == 'a') {
-        return 1
+    switch (word) {
+        case onePoint:
+            return 1
+        case twoPoint:
+            return 2
+        case threePoint:
+            return 3
+        case fourPoint:
+            return 4
+        case fivePoint:
+            return 5
+        case eightPoint:
+            return 8
+        case tenPoint:
+            return 10
     }
+    
 }
--- scrabble-score.test.js
+++ scrabble-score.test.js
@@ -1,15 +1,31 @@
-import { calculateScrabbleScore } from "./scrabble-score";
+import { calculateScrabbleScore, onePoint, twoPoint, threePoint, fourPoint, fivePoint, eightPoint, tenPoint } from "./scrabble-score";
 import { test, expect } from "vitest"
 
-const onePoint = ['a', 'e', 'i', 'l', 'n', 'o', 'r', 's', 't', 'u']
-const twoPoint = ['d', 'g']
-const threePoint = ['b', 'c', 'm', 'p']
-const fourPoint = ['f', 'h', 'v', 'w', 'y']
-const fivePoint = ['k']
-const eightPoint = ['j', 'x']
-const tenPoints = ['q', 'z']
-
 // test to make sure 'a' returns a score of 1
-test('the word "a" returns a score of 1', () => {
-    expect(calculateScrabbleScore('a')).toBe(1)
+test(onePoint, 'the one point letters returns a score of 1', (r) => {
+    expect(calculateScrabbleScore(r)).toBe(1)
+});
+
+test('the two point letters returns a score of 2', () => {
+    expect(calculateScrabbleScore(twoPoint)).toBe(2)
+})
+
+test('the three point letters returns a score of 3', () => {
+    expect(calculateScrabbleScore(threePoint)).toBe(3)
+})
+
+test('the four point letters returns a score of 4', () => {
+    expect(calculateScrabbleScore(fourPoint)).toBe(4)
+})
+
+test('the five point letters returns a score of 5', () => {
+    expect(calculateScrabbleScore(fivePoint)).toBe(5)
+})
+
+test('the eight point letters returns a score of 8', () => {
+    expect(calculateScrabbleScore(eightPoint)).toBe(8)
+})
+
+test('the ten point letters returns a score of 10', () => {
+    expect(calculateScrabbleScore(tenPoint)).toBe(10)
 })
